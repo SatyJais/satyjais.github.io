4084e063e0f3010f3c63437e51c2ab63043bf8e1
SatyJais <141466019+SatyJais@users.noreply.github.com>
2026-03-05 19:45:33 +0000

    Deploying to gh-pages from @ SatyJais/satyjais.github.io@3254941e765ef85376e1e845727eddbfc681f172 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -61,11 +61,6 @@ ninja.data = [{
           description: "ETL pipeline and dashboarding for Google &amp; Meta Ads using Google BigQuery(SQL) &amp; Looker Studio",
           section: "Projects",handler: () => {
               window.location.href = "/projects/BQ_Looker_Dashboard/";
-            },},{id: "projects-data-cleaning-amp-etl-automation-using-bigquery-sql-amp-google-sheets",
-          title: 'Data Cleaning &amp;amp; ETL Automation using BigQuery (SQL) &amp;amp; Google Sheets',
-          description: "Campaign report - data streamlining &amp; automation project",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/DataCleaning_ETL/";
             },},{id: "projects-bigquery-sql-amp-gsheets-data-ingestion-to-live-dashboard",
           title: 'BigQuery (SQL) &amp;amp; GSheets — Data Ingestion to Live Dashboard',
           description: "Streamlined campaign reporting workflow to improve accuracy, reduce manual effort, and enable faster decisions.",
